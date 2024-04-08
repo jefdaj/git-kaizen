5954908a9a4328e4d9b1e0170b8746e3c4322002
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-08 13:03:58 -0700

    move usage to docopt quasiquoter, add dummy example dhall task

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
 import Paths_kaizen
+import System.Environment (getArgs)
+import System.Console.Docopt
+
+patterns :: Docopt
+patterns = [docoptFile|usage.txt|]
+
+getArgOrExit = getArgOrExitWith patterns
 
 main :: IO ()
 main = do
-  usage <- readFile =<< getDataFileName "usage.txt"
-  putStrLn usage
+  args <- parseArgsOrExit patterns =<< getArgs
+  print args
