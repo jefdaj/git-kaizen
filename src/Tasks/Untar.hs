6f4692c7d7e1a7d74dd150595f3249ded6d19692
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-09 18:37:07 -0700

    todos

--- src/Tasks/Untar.hs
+++ src/Tasks/Untar.hs
@@ -15,6 +15,7 @@ task = Task
   }
 
 -- TODO generalize to "results" and have fields like "added" and "removed"?
+--      then this could be called preview
 outpaths :: [String] -> [String]
 outpaths inpaths = [dropExtension $ head inpaths]
 
