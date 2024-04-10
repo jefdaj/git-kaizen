13e6ea9d50e5c156d6491cc3ae4eef870bd82485
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-09 18:47:29 -0700

    wip: untar tests, misc

--- src/Main.hs
+++ src/Main.hs
@@ -22,15 +22,6 @@ patterns = [docoptFile|usage.txt|]
 
 getArgOrExit = getArgOrExitWith patterns
 
--- data Person = Person { age :: Natural, name :: Text }
---     deriving (Generic, FromDhall, Show)
-
--- cmdCheck :: FilePath -> IO ()
--- cmdCheck file = do
-  -- expr <- TIO.readFile file
-  -- (x :: Task) <- input auto expr
-  -- print $ name x
-
 tasks :: [Task]
 tasks =
   [ Untar.task
@@ -41,6 +32,3 @@ main = do
   args <- parseArgsOrExit patterns =<< getArgs
   print args
   putStrLn $ "tasks: " ++ show (map taskName tasks)
-  -- when (args `isPresent` (command "check")) $ do
-  --   file <- args `getArgOrExit` (argument "dhallfile")
-  --   cmdCheck file
