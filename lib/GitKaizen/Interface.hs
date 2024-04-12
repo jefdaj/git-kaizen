de9f5cdd8ab3674b13642b966e7164d45277704f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-12 10:36:40 -0700

    wip test suite: can find tests in lib modules

--- lib/GitKaizen/Interface.hs
+++ lib/GitKaizen/Interface.hs
@@ -1,8 +1,21 @@
-module GitKaizen.Interface
-  ( Kaizen(..)
-  , InPattern(..)
-  , Priority(..)
-  )
-  where
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module GitKaizen.Interface where
+  -- ( Kaizen(..)
+  -- , InPattern(..)
+  -- , Priority(..)
+  -- )
+  -- where
 
 import GitKaizen.Types
+
+import Test.Tasty
+import Test.Tasty.QuickCheck
+
+-- QuickCheck property
+prop_additionCommutative :: Int -> Int -> Bool
+prop_additionCommutative a b = a + b == b + a
+
+-- Tasty TestTree
+test_multiplication :: [TestTree]
+test_multiplication = [testProperty "One is identity" $ \(a :: Int) -> a * 1 == a]
