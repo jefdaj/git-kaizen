907ff11daed78226da2d97f868b34c01d0c62f05
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-05-30 17:19:39 -0700

    start bigtrees/kaizen.d/01_HashTarballs.hs

---
+++ examples/bigtrees/kaizen.d/01_HashTarballs.hs
@@ -0,0 +1,33 @@
+module GatherTarballs (kaizen) where
+
+import GitKaizen.Interface
+import System.FilePath.Posix (dropExtension)
+
+kaizen :: Kaizen
+kaizen = Kaizen
+  { kName = "bigtrees hash tarballs"
+  , kInPatterns = [GlobOne "*.tar*"]
+  , kOutPaths = outpaths
+  , kDescribe = desc
+  , kGuard = guard
+  , kCommand = undefined -- TODO write this
+  }
+
+-- TODO generalize to "results" and have fields like "added" and "removed"?
+--      then this could be called preview
+outpaths :: [String] -> [String]
+outpaths inpaths = [dropExtension $ head inpaths]
+
+-- TODO relativize paths?
+desc :: [String] -> [String] -> String
+desc inpaths outpaths = unwords
+  [ "extract from"
+  , "'" ++ head inpaths ++ "'"
+  , "->"
+  , "'" ++ head outpaths ++ "'"
+  ]
+
+-- TODO use asserts and return () rather than returning a bool?
+guard :: [String] -> [String] -> IO Bool
+guard inpaths outpaths = return $
+  (length inpaths  == 1) && (length outpaths == 1)
