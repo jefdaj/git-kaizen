8eaac113fa416c75dc967eb53b9ddb968de73d6e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-05-30 22:16:10 -0700

    wip rewriting Kaizen, starting bigtrees example

--- examples/bigtrees/kaizen.d/01_HashTarballs.hs
+++ examples/bigtrees/kaizen.d/01_HashTarballs.hs
@@ -1,4 +1,4 @@
-module GatherTarballs (kaizen) where
+module GatherTarballs where
 
 import GitKaizen.Interface
 import Control.Monad (filterM, forM)
@@ -11,7 +11,7 @@ kaizen = Kaizen
   { kDescription = "bigtrees hash tarballs"
 
   -- , kListInputs = [GlobOne "*.tar*"]
-  , kListInputs = \mp -> gitAnnexFind mp >>= notDone outpaths
+  , kListInputs = \mp -> gitAnnexFind mp >>= notDone listOutputs
 
   -- This takes one group of input files and returns the corresponding output files.
   -- If you define it as a named fn below, you can also use it as part of list inputs.
@@ -32,21 +32,11 @@ gitAnnexFind = undefined
 -- This should usually be the last thing in the list inputs pipeline,
 -- because you want the inputs properly grouped.
 notDone :: ListOutputsFn -> [[FilePath]] -> IO [[FilePath]]
--- notDone ps = filterM (\p -> (not . any) <$> doesFileExist $ outpaths p)
-notDone listOutputsFn inPathLists = flip filterM inPathLists $ \ps -> do
-  outPaths      <- listOutputsFn ps
-  outPathsExist <- mapM doesFileExist outPaths
-  return $ not $ all id outPathsExist
-
--- TODO think this through a bit more, seems forced with the mapping :/
-
--- TODO move this to Interface?
--- TODO rename to explain that this one takes ONE input path?
-outpathsAllExist :: FilePath -> IO Bool
-outpathsAllExist p = do
-  outs <- outpaths [p]
-  outsExist <- mapM doesFileExist outs
-  return $ all id outsExist
+notDone listOutputsFn inPathLists =
+  flip filterM inPathLists $ \ps -> do
+    outPaths <- listOutputsFn ps
+    outPathsExist <- mapM doesFileExist outPaths
+    return $ not $ all id outPathsExist
 
 -- TODO is this a standard fn already?
 -- TODO if not, move to Interface
@@ -56,5 +46,5 @@ singletons = undefined -- return $ map $ \p -> [p]
 
 -- TODO generalize to "results" and have fields like "added" and "removed"?
 --      then this could be called preview
-outpaths :: ListOutputsFn
-outpaths inpaths = return [dropExtension $ head inpaths]
+listOutputs :: ListOutputsFn
+listOutputs inpaths = return [dropExtension $ head inpaths]
