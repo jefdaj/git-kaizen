2ef9df5903e15853f8f05a8edfce387514828851
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-05-30 22:16:58 -0700

    wip rewriting Kaizen, starting bigtrees example

--- examples/bigtrees/kaizen.d/01_HashTarballs.hs
+++ examples/bigtrees/kaizen.d/01_HashTarballs.hs
@@ -11,7 +11,7 @@ kaizen = Kaizen
   { kDescription = "bigtrees hash tarballs"
 
   -- , kListInputs = [GlobOne "*.tar*"]
-  , kListInputs = \mp -> gitAnnexFind mp >>= notDone listOutputs
+  , kListInputs = \mp -> gitAnnexFind mp >>= notAllDone listOutputs
 
   -- This takes one group of input files and returns the corresponding output files.
   -- If you define it as a named fn below, you can also use it as part of list inputs.
@@ -31,10 +31,10 @@ gitAnnexFind = undefined
 -- TODO move to Interface
 -- This should usually be the last thing in the list inputs pipeline,
 -- because you want the inputs properly grouped.
-notDone :: ListOutputsFn -> [[FilePath]] -> IO [[FilePath]]
-notDone listOutputsFn inPathLists =
-  flip filterM inPathLists $ \ps -> do
-    outPaths <- listOutputsFn ps
+notAllDone :: ListOutputsFn -> [[FilePath]] -> IO [[FilePath]]
+notAllDone listOutputsFn inPathLists =
+  flip filterM inPathLists $ \inPaths -> do
+    outPaths <- listOutputsFn inPaths
     outPathsExist <- mapM doesFileExist outPaths
     return $ not $ all id outPathsExist
 
