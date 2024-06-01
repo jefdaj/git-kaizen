e1bc0aad6ba998c895023dfafd975302717ca6ea
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-05-31 17:15:57 -0700

    list outputs kind of works

--- examples/bigtrees/kaizen.d/hash-tarballs.sh
+++ examples/bigtrees/kaizen.d/hash-tarballs.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -x
+
+cd "$TMPDIR"
+
 INPATH="$1"
-OUTPATH="$(dirname "$INPATH")/$(basename "$INPATH" | cut -d'.' -f1)"
+OUTPATH="$(dirname "$INPATH")/$(basename "$INPATH" | cut -d'.' -f1)".bigtree
 
 # TODO can i define it in a way that shortens this?
-if [[ $GITKAIZEN_SCRIPT_MODE == LIST_OUTPUTS ]]; then
+if [[ $GITKAIZEN_RUN_MODE == LIST_OUTPUTS ]]; then
   echo "$OUTPATH"
   exit 0
 fi
